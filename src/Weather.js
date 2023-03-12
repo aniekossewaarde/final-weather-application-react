85b2b51927fec440116416dfcb40b5f9a956bba4
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2023-03-12 11:48:07 +0100

    cleaned up react data added bootstrap axios and animation

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+import axios from "axios";
+import "./Weather.css";
+import FormattedDate from "./FormattedDate";
+
+import './index.css';
+
+export default function Weather () {
+return <h1>Hello suckers</h1>
+}
