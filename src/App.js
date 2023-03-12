8183ab6e229acbea959c83d13f88fa516d7a80c1
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2023-03-12 11:59:58 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function App() {
       <div className="container">
           <header className="App-header">
         <Weather />
+        <footer>
+        <a href='https://github.com/aniekossewaarde/final-weather-application-react' target="_blank">Open-source code</a> by <a href='https://www.linkedin.com/in/aniek-ossewaarde-88a17085/?originalSubdomain=es' target="_blank">Aniek Ossewaarde</a>
+        </footer>
       </header>
       </div>
     </div>
