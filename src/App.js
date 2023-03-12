45130182934ba0cd52c2b527e7a2a1d2f68c6a3b
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2023-03-12 14:31:45 +0100

    added API integration

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <div className="container">
           <header className="App-header">
-        <Weather />
+        <Weather defaultCity="Amsterdam"/>
         <footer>
         <a href='https://github.com/aniekossewaarde/final-weather-application-react' target="_blank">Open-source code</a> by <a href='https://www.linkedin.com/in/aniek-ossewaarde-88a17085/?originalSubdomain=es' target="_blank">Aniek Ossewaarde</a>
         </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -5,40 +5,67 @@ import "./Weather.css";
 import FormattedDate from "./FormattedDate";
 
 import './index.css';
+import { clear } from "@testing-library/user-event/dist/clear";
 
-export default function Weather () {
-return <div className="Weather">
+export default function Weather(props) {
+    const [weatherData, setWeatherData] = useState({ ready: false });
+    function handleResponse(response) {
+setWeatherData({
+    ready: true,
+    temperature: response.data.main.temp,
+    wind: response.data.wind.speed,
+    description: response.data.weather[0].description,
+    city: response.data.name,
+    humidity: response.data.main.humidity,
+    date: "Sunday 14:30",
+    iconUrl: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAjdJREFUeNrtmsGtgzAMhjtCRmAERsgIHYFjjx2BERiBEToCI+TaG2+DbJBnKj8pD1Eaiv8AwpX+C1Ap/hzHjpNLCOFyZl0UgAJQAApAASgABaAAFIACODEA9C/83AypIrUkRwpv5Pib4dsCNp5cAMgIS3rMGPxJw3/t4QDQoEtSt8LwsTpJEFAAAONjNUM4HSIEOO7vJC8Mwa2FkHURZBDSM2KAWu4CwPP5LEgtyZMCy/OzIgKBgGA2BUAGliPDxxreldFM2EU4JAOgwRtSPeHdmj0/Z3z8fcEQasTCiATgEgxMURvNAkR2sOIA2MtBSH60KNbC4dAhAHhBACFDdrBiADj2A0DI7PA4AgB0dihWAaBBXUk92Hhkdqi+BsDGh8ySzg7tGgD9BgC88FrgvgKQIeY/ZgepxXB3m6EFm6bm7ADqQwDggSLUHQVA2KG6P50JQJ+a96UBuC08OwqRerOWGPftkUa7Oc/GrTBKo5b0mEivwzOLAlABjb8v6EY1CXVGgwCAampUqZ5NNP5fuS1aB6w87Zn1PG+XpStOKw3AIro5IONfM0e8EhRsZFxz7EYRACRmgQc0YPMAENrAdJk6UBgAKwojz/W/yQUgXgilARguS5dUd2Yir+foPzSQ7TAfifdLOjZvzhSzQID0A3gmuJTDzIQzRXg4IC9IvFsY68jzWxr/qgvQV2TsRJ1gwMXOIuW+JNWhjtx2DWCDE6fvAOhNUQWgABSAAlAACkABKAAFoABOp1+6Bd0LJ+BorgAAAABJRU5ErkJggg==`
+});
 
-<form>
-    <div className="row">
-        <div className="col-9">
-    <input type="search" placeholder="Enter a city..." className="form-control" />
-    </div>
-    <div className="col-3">
-    <input type="submit" value="Search" className="search-button w-100"/>
-    </div>
-    </div>
-</form>
 
-    <div className="current-location">
-    <h1>Barcelona</h1>
-    <ul>
-        <li>Last updated: Sunday 12:00</li>
-        <li>Description: Clear</li>
-    </ul>
-<div className="row">
-    <div className="col-6">
-        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAjdJREFUeNrtmsGtgzAMhjtCRmAERsgIHYFjjx2BERiBEToCI+TaG2+DbJBnKj8pD1Eaiv8AwpX+C1Ap/hzHjpNLCOFyZl0UgAJQAApAASgABaAAFIACODEA9C/83AypIrUkRwpv5Pib4dsCNp5cAMgIS3rMGPxJw3/t4QDQoEtSt8LwsTpJEFAAAONjNUM4HSIEOO7vJC8Mwa2FkHURZBDSM2KAWu4CwPP5LEgtyZMCy/OzIgKBgGA2BUAGliPDxxreldFM2EU4JAOgwRtSPeHdmj0/Z3z8fcEQasTCiATgEgxMURvNAkR2sOIA2MtBSH60KNbC4dAhAHhBACFDdrBiADj2A0DI7PA4AgB0dihWAaBBXUk92Hhkdqi+BsDGh8ySzg7tGgD9BgC88FrgvgKQIeY/ZgepxXB3m6EFm6bm7ADqQwDggSLUHQVA2KG6P50JQJ+a96UBuC08OwqRerOWGPftkUa7Oc/GrTBKo5b0mEivwzOLAlABjb8v6EY1CXVGgwCAampUqZ5NNP5fuS1aB6w87Zn1PG+XpStOKw3AIro5IONfM0e8EhRsZFxz7EYRACRmgQc0YPMAENrAdJk6UBgAKwojz/W/yQUgXgilARguS5dUd2Yir+foPzSQ7TAfifdLOjZvzhSzQID0A3gmuJTDzIQzRXg4IC9IvFsY68jzWxr/qgvQV2TsRJ1gwMXOIuW+JNWhjtx2DWCDE6fvAOhNUQWgABSAAlAACkABKAAFoABOp1+6Bd0LJ+BorgAAAABJRU5ErkJggg==" alt="weather-icon" />
-        <span className="temperature">17</span><span className="units">°C</span>
-    </div>
-    <div className="col-6">
-        <ul className="information">
-            <li>Humidity: 69%</li>
-            <li>Wind: 16km/h</li>
-        </ul>
-    </div>
-</div>
+    }
 
-    </div>
-</div>
+    if (weatherData.ready) {
+        return ( 
+            <div className="Weather">
+            
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                <input type="search" placeholder="Enter a city..." className="form-control" />
+                </div>
+                <div className="col-3">
+                <input type="submit" value="Search" className="search-button w-100"/>
+                </div>
+                </div>
+            </form>
+            
+                <div className="current-location">
+                <h1>{weatherData.city}</h1>
+                <ul>
+                    <li>Last updated: {weatherData.date}</li>
+                    <li className="text-capitalize">{weatherData.description}</li>
+                </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src={weatherData.iconUrl} alt={weatherData.description} />
+                    <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="units">°C</span>
+                </div>
+                <div className="col-6">
+                    <ul className="information">
+                        <li>Humidity: {weatherData.humidity}%</li>
+                        <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+                    </ul>
+                </div>
+            </div>
+            
+                </div>
+            </div>
+            );
+    } else {
+        let apiKey = "ab69138d433c5c85edafefc09e466c07";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+    }
 }
