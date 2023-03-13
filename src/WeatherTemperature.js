941ed420411117fda0e0ddf5d0478a08ea12086f
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2023-03-13 21:18:06 +0100

    created forecast for searched city

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,34 +1,18 @@
 import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-    const [unit, setUnit] = useState("celsius");
 
-function convertToFahrenheit(event) {
-event.preventDefault();
-setUnit("fahrenheit");
-}
 
-function convertToCelsius(event) {
-    event.prevenDefault();
-    setUnit("celsius");
-}
 
-    if (unit === "celsius") {
+
+    
 return (
-    <span className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="units">°C | <a href="/" onClick={convertToFahrenheit}>°F</a></span>
-        
-    </span>
-);
-} else {
-    let fahrenheit = (props.celsius * 9/5) + 32; 
-    return (
+    
         <span className="WeatherTemperature">
-            <span className="temperature">{Math.round(fahrenheit)}</span>
-            <span className="units"><a href="/" onClick={convertToCelsius}>°C</a> | °F</span>
+            <span className="temperature">{Math.round(props.celsius)}</span>
+            <span className="units">°C</span>
             
         </span>
     );
-}
+
 }
